dd50c866a0ceaa77e89099acfde24339d582c203
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-06-24 11:27:06 +0200

    refactor: next aspects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/train-trip-generator-5000-plus/' : '/',
-  optimizeDeps: {
-    include: ['db-input', 'db-button']
-  }
+  base: mode === 'production' ? '/train-trip-generator-5000-plus/' : '/'
 }));
