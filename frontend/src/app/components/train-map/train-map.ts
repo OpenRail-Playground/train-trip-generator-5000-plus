0c2bc7b3a60576e829c66089ad72058bd85959a9
Erik Rijcken <erik.rijcken@infrabel.be>
2025-06-24 23:01:11 +0200

    Dynamically draw the train map based on the trip response

--- frontend/src/app/components/train-map/train-map.ts
+++ frontend/src/app/components/train-map/train-map.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import * as L from 'leaflet';
+import { TripResponse, TripResponsePart } from '../../models/trip-response';
 
 @Component({
 	selector: 'app-train-map',
@@ -10,30 +11,44 @@ export class TrainMapComponent implements AfterViewInit {
 	@ViewChild('map', { static: true }) mapElement!: ElementRef<HTMLDivElement>;
 	private map!: L.Map;
 
+	@Input() tripResponse: TripResponse | undefined;
+
 	ngAfterViewInit(): void {
 		this.initMap();
 	}
 
 	private initMap(): void {
-		// Coordinates for Copenhagen and Paris
-		const copenhagen: L.LatLngExpression = [55.6761, 12.5683];
-		const paris: L.LatLngExpression = [48.8566, 2.3522];
-
 		// Initialize the map centered roughly in Western Europe
 		this.map = L.map(this.mapElement.nativeElement).setView([52.0, 7.0], 5);
-
+		
 		// Add OpenStreetMap tiles
 		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			maxZoom: 19,
 			attribution: '© OpenStreetMap contributors',
 		}).addTo(this.map);
+		
+		if (!this.tripResponse || !this.tripResponse.trips) {
+			return;
+		}
 
-		// Add markers
-		L.marker(copenhagen).addTo(this.map).bindPopup('Copenhagen');
-		L.marker(paris).addTo(this.map).bindPopup('Paris');
+		let points: any = [];
+		for (let i = 0; i < this.tripResponse.trips.length; i++){
+			// Add markers
+			let part = this.tripResponse?.trips[i];
+
+			let pt1: L.LatLngExpression = [part.departureStationLatitude, part.departureStationLongitude];
+			L.marker(pt1).addTo(this.map).bindPopup(part.departureStationName);
+			points.push(pt1);
+			
+			if (i == this.tripResponse.trips.length-1) {
+				let pt2: L.LatLngExpression = [part.arrivalStationLatitude, part.arrivalStationLongitude];
+				L.marker(pt2).addTo(this.map).bindPopup(part.departureStationName);
+				points.push(pt2);
+			}
+		}
 
 		// Draw train route polyline
-		const trainRoute = L.polyline([copenhagen, paris], {
+		const trainRoute = L.polyline(points, {
 			color: 'blue',
 			weight: 4,
 			opacity: 0.7,
