9a6af5135411ae947855d00a009b138122d3d835
Gerstbauer Johannes (BCC.ICT) <Johannes.Gerstbauer@oebb.at>
2025-06-24 13:49:50 +0200

    Add basic endpoint

--- backend/src/main/java/ch/hack4rail/traintipgenerator/controller/StopController.java
+++ backend/src/main/java/ch/hack4rail/traintipgenerator/controller/StopController.java
@@ -1,18 +1,29 @@
 package main.java.ch.hack4rail.traintipgenerator.controller;
 
-import main.java.ch.hack4rail.traintipgenerator.request.StopRequest;
+import main.java.ch.hack4rail.traintipgenerator.request.AutocompleteRequest;
+import main.java.ch.hack4rail.traintipgenerator.request.TripRequest;
+import main.java.ch.hack4rail.traintipgenerator.response.TrainTrip;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class StopController {
 
-    @PostMapping("/stop")
-    public String getStop(@RequestBody StopRequest request) {
+    @PostMapping("/autocomplete")
+    public String getAutocomplete(@RequestBody AutocompleteRequest request) {
         return "Hallo, " + request.stationName() + "!";
     }
+
+    @PostMapping("/trip")
+    public List<TrainTrip> getTrip(@RequestBody TripRequest request) {
+        return new ArrayList<>();
+    }
+
 
 }
---
+++ backend/src/main/java/ch/hack4rail/traintipgenerator/response/TrainTrip.java
@@ -0,0 +1,6 @@
+package main.java.ch.hack4rail.traintipgenerator.response;
+
+public record TrainTrip(
+        String stationName
+) {
+}
