8b51a17ac587652badf28d1b6dd451eece052e56
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-06-24 14:56:00 +0200

    refactor: running prettier

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,41 +2,41 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import {
-  DBButton,
-  DBCard,
-  DBInput,
-  DBHeader,
-  DBBrand,
-  DBLink,
-  DBNavigation,
-  DBNavigationItem,
-  NavigationContentDirective,
-  NavigationDirective,
-  SecondaryActionDirective,
-  MetaNavigationDirective,
+	DBButton,
+	DBCard,
+	DBInput,
+	DBHeader,
+	DBBrand,
+	DBLink,
+	DBNavigation,
+	DBNavigationItem,
+	NavigationContentDirective,
+	NavigationDirective,
+	SecondaryActionDirective,
+	MetaNavigationDirective,
 } from '@db-ux/ngx-core-components';
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    DBButton,
-    DBCard,
-    DBInput,
-    DBHeader,
-    DBBrand,
-    DBLink,
-    DBNavigation,
-    DBNavigationItem,
-    NavigationContentDirective,
-    NavigationDirective,
-    SecondaryActionDirective,
-    MetaNavigationDirective,
-  ],
-  standalone: true,
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+	selector: 'app-root',
+	imports: [
+		RouterOutlet,
+		DBButton,
+		DBCard,
+		DBInput,
+		DBHeader,
+		DBBrand,
+		DBLink,
+		DBNavigation,
+		DBNavigationItem,
+		NavigationContentDirective,
+		NavigationDirective,
+		SecondaryActionDirective,
+		MetaNavigationDirective,
+	],
+	standalone: true,
+	templateUrl: './app.html',
+	styleUrl: './app.css',
 })
 export class App {
-  protected title = 'train-trip-generator-5000-plus-frontend';
+	protected title = 'train-trip-generator-5000-plus-frontend';
 }
