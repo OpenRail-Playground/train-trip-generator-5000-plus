c8e2cc4832c1b6a8c0b2ab740b85fe54a36abf2c
Gerstbauer Johannes (BCC.ICT) <Johannes.Gerstbauer@oebb.at>
2025-06-24 17:57:29 +0200

    Add response object

--- backend/src/main/java/ch/hack4rail/traintripgenerator/response/TripResponsePart.java
+++ backend/src/main/java/ch/hack4rail/traintripgenerator/response/TripResponsePart.java
@@ -1,7 +1,16 @@
 package ch.hack4rail.traintripgenerator.response;
 
+import java.time.LocalDateTime;
+
 public record TripResponsePart(
-        Long id,
-        String name
+        float departureStationLatitude,
+        float departureStationLongitude,
+        float arrivalStationLatitude,
+        float arrivalStationLongitude,
+        String departureStationName,
+        String arrivalStationName,
+        LocalDateTime departureTime,
+        LocalDateTime arrivalTime,
+        String trainName
 ) {
 }
