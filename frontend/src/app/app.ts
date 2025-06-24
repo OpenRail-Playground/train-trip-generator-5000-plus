1414537c191fc820b658ed63c545d5edc8d7c142
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-06-24 13:38:43 +0200

    refactor: adding further imports

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { DBButton, DBCard, DBInput, DBHeader, DBBrand, DBLink, DBNavigation, DBNavigationItem } from '@db-ux/ngx-core-components';
+import {
+  DBButton,
+  DBCard,
+  DBInput,
+  DBHeader,
+  DBBrand,
+  DBLink,
+  DBNavigation,
+  DBNavigationItem,
+  NavigationContentDirective,
+  NavigationDirective,
+  SecondaryActionDirective,
+  MetaNavigationDirective,
+} from '@db-ux/ngx-core-components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DBButton, DBCard, DBInput, DBHeader, DBBrand, DBLink, DBNavigation, DBNavigationItem],
+  imports: [
+    RouterOutlet,
+    DBButton,
+    DBCard,
+    DBInput,
+    DBHeader,
+    DBBrand,
+    DBLink,
+    DBNavigation,
+    DBNavigationItem,
+    NavigationContentDirective,
+    NavigationDirective,
+    SecondaryActionDirective,
+    MetaNavigationDirective,
+  ],
   standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'train-trip-generator-5000-plus-frontend';
